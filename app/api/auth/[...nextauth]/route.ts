fb4ced48a16d88e8e6cabfa8a794c97cd4c86ceb
minhanPark <alsgks1218@naver.com>
2023-05-31 18:24:23 +0900

    구글 프로바이더까지 추가

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/app/libs/prismadb";
@@ -8,6 +9,10 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,18 @@ export default function Home() {
   return (
     <div>
       <h1>메인 페이지</h1>
-      <button onClick={() => signIn("github")}>깃허브 로그인</button>
+      <button
+        className="border border-lime-100 border-1"
+        onClick={() => signIn("github")}
+      >
+        깃허브 로그인
+      </button>
+      <button
+        className="border border-lime-100 border-1"
+        onClick={() => signIn("google")}
+      >
+        구글 로그인
+      </button>
     </div>
   );
 }
